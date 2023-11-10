584fc110d20b29a209d3e721b8f8a69cb6d25825
khant-min <kmin01405@gmail.com>
2023-11-10 11:22:11 +0630

    finish crud with ado dot net

--- KMDotNetCore/AdoDotNetExamples/AdoDotNetExample.cs
+++ KMDotNetCore/AdoDotNetExamples/AdoDotNetExample.cs
@@ -10,27 +10,30 @@ namespace KMDotNetCore.ConsoleApp.AdoDotNetExamples
 {
     public class AdoDotNetExample
     {
+        private readonly SqlConnectionStringBuilder sqlConnectionStringBuilder;
 
-        public void Run(string runCmd, string title = null, string author = null, string content = null)
+        public AdoDotNetExample()
         {
-            if (runCmd == "read")
-            {
-                Read();
-            } else
-            {
-                Create(title, author, content);
-            }
-        }
-       
-        private void Read()
-        {
-            SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+            sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
             {
                 DataSource = ".",
                 InitialCatalog = "ALTDotNetCore",
                 UserID = "sa",
                 Password = "sa@123"
             };
+        }
+
+        public void Run()
+        {
+            Read();
+            Create("test1", "test 2", "test 3");
+            Edit(1);
+            Update(1, "test 6", "test 7", "test 8");
+            Delete(1);
+        }
+       
+        private void Read()
+        { 
             SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
             Console.WriteLine("Connection opening...");
             connection.Open();
@@ -61,15 +64,35 @@ namespace KMDotNetCore.ConsoleApp.AdoDotNetExamples
             }
         }
 
+        private void Edit(int id)
+        {
+            SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+
+            string query = "select * from tbl_blog where Blog_Id = @Blog_Id;";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@Blog_Id", id);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            connection.Close();
+
+            if (dt.Rows.Count == 0)
+            {
+                Console.WriteLine("No data found");
+                return;
+            }
+            DataRow row = dt.Rows[0];
+
+            Console.WriteLine(row["Blog_Id"]);
+            Console.WriteLine(row["Blog_Title"]);
+            Console.WriteLine(row["Blog_Author"]);
+            Console.WriteLine(row["Blog_Content"]);
+        }
+
         private void Create(string title, string author, string content)
         {
-            SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
-            {
-                DataSource = ".",
-                InitialCatalog = "ALTDotNetCore",
-                UserID = "sa",
-                Password = "sa@123"
-            };
             SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
             connection.Open();
 
@@ -94,6 +117,48 @@ namespace KMDotNetCore.ConsoleApp.AdoDotNetExamples
             Console.WriteLine(message);
         }
 
+        private void Update(int id, string title, string author, string content)
+        {
+            SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+
+            string query = @"
+                            UPDATE [dbo].[Tbl_Blog]
+                            SET [Blog_Title] = @Blog_Title
+                                ,[Blog_Author] = @Blog_Author
+                                ,[Blog_Content] = @Blog_Content
+                            WHERE Blog_Id = @Blog_Id";
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@Blog_Id", id);
+            cmd.Parameters.AddWithValue("@Blog_Title", title);
+            cmd.Parameters.AddWithValue("@Blog_Author", author);
+            cmd.Parameters.AddWithValue("@Blog_Content", content);
+            int result = cmd.ExecuteNonQuery();
+
+            connection.Close();
+            string message = result > 0 ? "Updating successful." : "Updating failed!";
+            Console.WriteLine(message);
+        }
+
+        private void Delete(int id)
+        {
+            SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+
+            string query =  @"
+                            DELETE FROM [dbo].[Tbl_Blog]
+                            WHERE Blog_Id = @Blog_Id";
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@Blog_Id", id);
+            int result = cmd.ExecuteNonQuery();
+
+            connection.Close();
+            string message = result > 0 ? "Delete successful." : "Delete failed!";
+            Console.WriteLine(message);
+
+        }
 
     }
 }
--- KMDotNetCore/Program.cs
+++ KMDotNetCore/Program.cs
@@ -3,7 +3,6 @@
 using KMDotNetCore.ConsoleApp.AdoDotNetExamples;
 
 AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
-adoDotNetExample.Run("read");
-adoDotNetExample.Run("create", "title 1", "author 1", "content 1");
+adoDotNetExample.Run();
 
 Console.ReadLine();
