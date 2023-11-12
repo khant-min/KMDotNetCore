ea11b751c67a96eb8819d0d99394db27461cfcaa
khant-min <kmin01405@gmail.com>
2023-11-12 20:55:35 +0630

    finish read and edit using dapper

---
+++ KMDotNetCore/DapperExamples/DapperExample.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using KMDotNetCore.ConsoleApp.Models;
+
+namespace KMDotNetCore.ConsoleApp.DapperExamples
+{
+    internal class DapperExample
+    {
+        private readonly SqlConnectionStringBuilder sqlConnectionStringBuilder;
+
+        public DapperExample()
+        {
+            sqlConnectionStringBuilder = new SqlConnectionStringBuilder
+            {
+                DataSource = ".",
+                InitialCatalog = "ALTDotNetCore",
+                UserID = "sa",
+                Password = "sa@123"
+            };
+        }
+
+        public void Run()
+        {
+            Read();
+            Edit(5);
+        }
+
+        private void Read()
+        {
+            string query = "select * from tbl_blog";
+            using IDbConnection db = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+            List<BlogDataModel> lst = db.Query<BlogDataModel>(query).ToList();
+
+            foreach (BlogDataModel item in lst)
+            {
+                Console.WriteLine(item.Blog_Id);
+                Console.WriteLine(item.Blog_Title);
+                Console.WriteLine(item.Blog_Author);
+                Console.WriteLine(item.Blog_Content);
+            }
+        }
+
+        private void Edit(int id)
+        {
+            BlogDataModel blog = new BlogDataModel()
+            {
+                Blog_Id = id
+            };
+
+            string query = "select * from tbl_blog where Blog_Id = @Blog_Id";
+            using IDbConnection db = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+            //BlogDataModel? item = db.Query<BlogDataModel>(query, new { Blog_Id = id }).FirstOrDefault();
+            BlogDataModel? item = db.Query<BlogDataModel>(query, blog).FirstOrDefault();
+
+            if (item is null)
+            {
+                Console.WriteLine("No data found.");
+                return;
+            }
+            Console.WriteLine(item.Blog_Id);
+            Console.WriteLine(item.Blog_Title);
+            Console.WriteLine(item.Blog_Author);
+            Console.WriteLine(item.Blog_Content);
+        }
+    }
+}
--- KMDotNetCore/Program.cs
+++ KMDotNetCore/Program.cs
@@ -1,8 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using KMDotNetCore.ConsoleApp.AdoDotNetExamples;
+using KMDotNetCore.ConsoleApp.DapperExamples;
 
-AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
-adoDotNetExample.Run();
+//AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+//adoDotNetExample.Run();
+DapperExample dapperExample = new DapperExample();
+dapperExample.Run();
 
 Console.ReadLine();
