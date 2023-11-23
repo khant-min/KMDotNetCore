62178b5f6d41f891747491872316d2f3201aff9c
khant-min <kmin01405@gmail.com>
2023-11-23 22:07:59 +0630

    init dot net core mvc

---
+++ KMDotNetCore.MvcApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KMDotNetCore.MvcApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
