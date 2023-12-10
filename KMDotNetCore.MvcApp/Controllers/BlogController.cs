a5eaa17109c32f6ee3f1f0a12c61adf4412e4fd7
khant-min <kmin01405@gmail.com>
2023-12-10 21:17:02 +0630

    complete MVC blog read & create

--- KMDotNetCore.MvcApp/Controllers/BlogController.cs
+++ KMDotNetCore.MvcApp/Controllers/BlogController.cs
@@ -1,17 +1,47 @@
-﻿using KMDotNetCore.MvcApp.Models;
+﻿using KMDotNetCore.MvcApp.EFDbContext;
+using KMDotNetCore.MvcApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KMDotNetCore.MvcApp.Controllers
 {
     public class BlogController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public BlogController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         [ActionName("Index")]
         public IActionResult BlogIndex()
         {
-            AppDbContext db = new AppDbContext();
-            List<BlogDataModel> lst = db.Blogs.ToList();
+            List<BlogDataModel> lst = _context.Blogs.ToList();
 
             return View("BlogIndex", lst);
+        }
+
+        [ActionName("Create")]
+        public IActionResult BlogCreate()
+        {
+            return View("BlogCreate");
+        }
+
+        [HttpPost]
+        [ActionName("Save")]
+        public async Task<IActionResult> BlogSave(BlogDataModel reqModel)
+        {
+            await _context.Blogs.AddAsync(reqModel);
+            var result = await _context.SaveChangesAsync();
+            string message = result > 0 ? "Saving successful" : "Saving failed";
+            TempData["Message"] = message;
+            TempData["isSuccess"] = result > 0;
+
+            // ViewBag
+            // ViewData
+            // TempData
+            // Session
+            return Redirect("/blog");
         }
     }
 }
---
+++ KMDotNetCore.MvcApp/EFDbContext/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KMDotNetCore.MvcApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KMDotNetCore.MvcApp.EFDbContext
+{
+    public class AppDbContext : DbContext
+    {
+      public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<BlogDataModel> Blogs { get; set; }
+    }
+}
