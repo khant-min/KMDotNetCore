29d8c3205f6398f23a4f28e3a36da87df7d0d79c
Khant Min Thu <kmin01405@gmail.com>
2024-01-02 18:43:02 +0630

    [Complete] theme dashboard

---
+++ KMDotNetCore.ThemeMvcApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KMDotNetCore.ThemeMvcApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
